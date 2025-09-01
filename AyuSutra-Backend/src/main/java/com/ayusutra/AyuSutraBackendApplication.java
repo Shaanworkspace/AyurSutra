02afd85872d868ebbbb859bcf5ac9ca6186537af
shaanworkspace <shaanyworkspace@gmail.com>
2025-09-01 21:39:15 +0530

    added initial setups

---
+++ AyuSutra-Backend/src/main/java/com/ayusutra/AyuSutraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ayusutra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyuSutraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyuSutraBackendApplication.class, args);
+	}
+
+}
---
+++ AyuSutra-Backend/src/test/java/com/ayusutra/AyuSutraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayusutra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyuSutraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
