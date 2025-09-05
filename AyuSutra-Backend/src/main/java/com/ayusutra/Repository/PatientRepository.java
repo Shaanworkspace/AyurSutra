bbc79e68eb41f82707dd936286bbd57bfc615efd
Shaanworkspace <shaanyworkspace@gmail.com>
2025-09-05 14:57:39 +0530

    made it only frontend

--- AyuSutra-Backend/src/main/java/com/ayusutra/Repository/PatientRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ayusutra.Repository;
-import com.ayusutra.Entity.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
--- AyuSutra-Backend/src/main/java/com/ayusutra/Repository/TherapyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ayusutra.Repository;
-
-import com.ayusutra.Entity.Therapy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TherapyRepository extends JpaRepository<Therapy, Long> {
-}
